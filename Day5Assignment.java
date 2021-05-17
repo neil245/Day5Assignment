64827fb3d719fc660bc5a3990621eb84c67279ce
neil245 <indranilghoshal5@gmail.com>
2021-05-17 16:19:33 +0530

    New file added

--- Day5Assignment.java
+++ Day5Assignment.java
@@ -4,11 +4,24 @@ class Welcome {
 	System.out.println("Welcome");
 	}
 }
+class Absent_Present {
+	public void check() {
+		int attendance = (int)Math.floor(Math.random()*2);
+		if (attendance == 0) {
+			System.out.println("Employee is absent");
+		}
+		else {
+			System.out.println("Employee is present");
+		}
+	}
+}
 public class Day5Assignment {
 	public static void main(String[] args) {
 		
 		Welcome obj = new Welcome();
 		obj.display();
+		Absent_Present obj1 = new Absent_Present();
+		obj1.check();
 	}
 
 }
