eeff26f9e2e87cf6a622a0d48345e8d3ad209501
neil245 <indranilghoshal5@gmail.com>
2021-05-17 23:55:46 +0530

    Files added

--- Day5Assignment.java
+++ Day5Assignment.java
@@ -6,7 +6,7 @@ class Welcome {
 }
 class Absent_Present {
 	public void check() {
-		int attendance = (int)Math.floor(Math.random()*2);
+		public int attendance = (int)Math.floor(Math.random()*2);
 		if (attendance == 0) {
 			System.out.println("Employee is absent");
 		}
@@ -34,17 +34,51 @@ class Part_time_wage {
 		System.out.println("Part time wages is " + parttimesal);
 	}
 }
+class Switch {
+	final int sal = 20;
+	 final int hours = 8;
+	 final int part_time_hours = 4;
+    void choice() {
+	int attendance = (int)Math.floor(Math.random()*3); 
+	System.out.println(attendance);
+	switch(attendance)
+	{
+	case 0:
+		System.out.println("Employee is absent");
+		break;
+	case 1:
+		System.out.println("Employee is part time present");
+		int part_time_salary = sal*part_time_hours;
+		System.out.println(part_time_salary);
+	break;
+	case 2:
+		System.out.println("Employee is full time present");
+		int total_salary = sal*hours;
+		System.out.println(total_salary);
+	break;
+	default:
+	System.out.println("Invalid output");
+	break;
+	
+	
+	
+}
+}
+}
+
 public class Day5Assignment {
 	public static void main(String[] args) {
 		
-		Welcome obj = new Welcome();
-		obj.display();
-		Absent_Present obj1 = new Absent_Present();
-		obj1.check();
-		Wage_Calculator obj2 = new Wage_Calculator();
-		obj2.calculate();
-		Part_time_wage obj3 = new Part_time_wage();
-		obj3.part_time_wage();
+		//Welcome obj = new Welcome();
+		//obj.display();
+		//Absent_Present obj1 = new Absent_Present();
+		//obj1.check();
+		//Wage_Calculator obj2 = new Wage_Calculator();
+		//obj2.calculate();
+		//Part_time_wage obj3 = new Part_time_wage();
+		//obj3.part_time_wage();
+		Switch obj4 = new Switch();
+		obj4.choice();
 	}
 
 }
