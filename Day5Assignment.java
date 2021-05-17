0eecbdbe8271dd29d294f6801ceceea8aaf2a88c
neil245 <indranilghoshal5@gmail.com>
2021-05-17 16:11:33 +0530

    New file added

---
+++ Day5Assignment.java
@@ -0,0 +1,14 @@
+package com.Day5.java;
+class Welcome {
+	public void display() {
+	System.out.println("Welcome");
+	}
+}
+public class Day5Assignment {
+	public static void main(String[] args) {
+		
+		Welcome obj = new Welcome();
+		obj.display();
+	}
+
+}
